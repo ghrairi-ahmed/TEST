c1bfe62f2ace7c55664c2ab2bf0b3b3ca615c065
ghrairi-ahmed <ghrayriahmed88@gmail.com>
2022-12-04 10:13:04 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 import Profile from "./profile/Profile";
 
 function App() {
+  const[show, setShow] = useState(false);
   return (
     <div className="App">
-      <Profile myprofession="I Learn Development Web At Go My Code"  mybio="Im 19"  name="Ahmed" lastName="Ghrairi">
-        <img className="alo" src="../ahmed.jpg" alt="Ahmed" />
+      <button onClick={()=>setShow(!show)}>Show profile </button>
+     {show ? <Profile myprofession="I Learn Development Web At Go My Code"  mybio="Im 19"  name="Ahmed" lastName="Ghrairi">
+        <img className="alo" src="../ahmed.jpg" alt="Ahmed" /> </Profile> : null}
         
-      </Profile>
+     
     </div>
   );
 }
